40361750a4b5e979e9a962315a54c585763a9b99
Abraham Vidal <antonio.vidal95@hotmail.com>
2025-05-20 22:15:20 -0400

    petId added un share profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,21 +4,31 @@ import { useEffect } from "react";
 
 const Profile = () => {
   let username: string | null = null;
+  let petId: string | null = null;
   if (typeof window !== "undefined") {
     username = new URLSearchParams(window.location.search).get("username");
+    petId = new URLSearchParams(window.location.search).get("petId");
   }
 
   console.log("username", username);
+  console.log("petId", petId);
   useEffect(() => {
     if (!username) return;
-    openMobileApp({ key: "profile", value: username });
-  }, [username]);
+    openMobileApp({
+      key: "profile",
+      value: `${username}${petId ?? `?petId=${petId}`}`,
+    });
+  }, [username, petId]);
+
   return (
     <div style={{ marginTop: "300px", textAlign: "center" }}>
       <h1>Abriendo la app...</h1>
       <p>
-        Si no se abre,{" "}
-        <a href={`patitas://profile/${username}`}>haz clic aquí</a>.
+        Si no se abre,
+        <a href={`patitas://profile/${username}${petId ?? `?petId=${petId}`}`}>
+          haz clic aquí
+        </a>
+        .
       </p>
     </div>
   );
